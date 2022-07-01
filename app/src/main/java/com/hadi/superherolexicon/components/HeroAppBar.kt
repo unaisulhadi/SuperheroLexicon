62bf7df53f43d7fa35046881f51b7c6565bccedf
Unaisul Hadi <unaisulhadi@gmail.com>
2022-07-01 18:41:18 +0530

    Coil Update

--- app/src/main/java/com/hadi/superherolexicon/components/HeroAppBar.kt
+++ app/src/main/java/com/hadi/superherolexicon/components/HeroAppBar.kt
@@ -11,10 +11,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import com.hadi.superherolexicon.R
 import com.hadi.superherolexicon.data.model.Hero
 import com.hadi.superherolexicon.ui.theme.accentColor
 
@@ -22,25 +26,25 @@ import com.hadi.superherolexicon.ui.theme.accentColor
 @Composable
 fun HeroAppBar(hero: Hero) {
 
+    val builder = ImageRequest.Builder(LocalContext.current)
+
     Box(
         modifier = Modifier
-            .height(370.dp)
+            .height(450.dp)
     ) {
 
-
-        Image(
-            painter = rememberImagePainter(
-                data = hero.images?.lg,
-                builder = {
-                    crossfade(true)
-                }
-            ),
+        AsyncImage(
+            model = builder.data(data = hero.images?.lg)
+                .placeholder(R.drawable.app_placeholder)
+                .error(R.drawable.app_placeholder)
+                .build(),
             modifier = Modifier
                 .fillMaxWidth()
-                .height(370.dp),
+                .fillMaxHeight(),
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
+
 
         Box(
             modifier = Modifier
